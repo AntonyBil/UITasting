e805b2c1641018cc0f4846e39591b27a141d2a6d
AntonyBil <beletskiy.ant@gmail.com>
2023-06-15 22:44:12 +0300

    Write test for adding name

--- UITestingUITests/UITestingUITests.swift
+++ UITestingUITests/UITestingUITests.swift
@@ -9,5 +9,24 @@ import XCTest
 
 final class UITestingUITests: XCTestCase {
 
-    
+    func testAddNoiseMaker() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        let newNameBtn = app.navigationBars["Noise Makers"].buttons["Item"]
+        let nameField = app.alerts["Add Noise Maker"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Shaggy"]/*[[".cells.textFields[\"Shaggy\"]",".textFields[\"Shaggy\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let addBtn = app.alerts["Add Noise Maker"].scrollViews.otherElements.buttons["Add"]
+        let addedCell = app.tables/*@START_MENU_TOKEN@*/.staticTexts["Antony"]/*[[".cells.staticTexts[\"Antony\"]",".staticTexts[\"Antony\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        newNameBtn.tap()
+        nameField.tap()
+        nameField.typeText("Antony")
+        
+        XCTAssertFalse(addedCell.exists)
+        
+        addBtn.tap()
+        XCTAssertTrue(addedCell.exists)
+       
+    }
 }
